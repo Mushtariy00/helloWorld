4ba11c675587a98aed72b46ee240aca670ec617c
Mushtariy Abdurakhmonova <mushtariy@Mushtariys-MacBook-Air.local>
2022-04-02 00:10:31 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,112 @@
+#print("I love pizza")
+#print("It's really good!")
+#name = "Anna"
+#print(type(name))
+#first_name = "Anna"
+#last_name = "Jeffer"
+#full_name = first_name + " "+ last_name
+#print("Hello "+full_name)
+#age = 22
+#age += 1
+#print("Your age is: "+str(age))
+#print(type(age))
+
+
+#height = 250.5
+#print("Your height is: "+str(height)+"cm")
+#print(type(height))
+
+#human= True
+#print("Are you a human: "+str(human))
+#print(type(human))
+
+#name = "Mushtariy"
+#age = 22
+#attractive = True
+
+#name, age, attractive = "Mushtariy", 21, True
+#print(name)
+#print(age)
+#print(attractive)
+
+#Spongebob = 30
+#Patrick = 30
+#Sandy = 30
+#Squidward = 30
+#Spongebob = Patrick = Sandy = Squidward = 30
+
+#print(Spongebob)
+#print(Patrick)
+#print(Sandy)
+#print(Squidward)
+
+#name = "MUSHTariy"
+#print(len(name))
+#print(name.find("y"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("m"))
+#print(name.replace("r","s"))++++++
+#print (name* 4)
+
+#type casting
+#x = 1  #int
+#y = 2.0 #float
+#z = "3" #str
+
+#x = str(x)
+#y = str(y)
+#z = str(z)
+
+#print("x is " +x)
+#print("y is " +y)
+#print(z*3)
+
+#name= input("What is your name? ")
+#age = int(input("How old are you?: "))
+#height = float(input("How tall are you?: "))
+#age = age + 1
+
+#print("Hello "+name)
+#print("You are "+str(age)+" years old")
+#print("You are "+str(height)+"cm tall")
+
+import math
+
+pi = -3.14
+
+#print(round(pi))
+#print(math.ceil(pi))
+#print(math.floor(pi))
+print(abs(pi))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
